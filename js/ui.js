90af321037035eb8957101c69b1b4279d9ab51db
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-11-27 19:40:26 +1100

    Documentation

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,23 @@
+
+/**
+ * Init user interface of the simulation
+ *
+ * @param  {function} onClick Function that is called when user submits
+ *                            commands. Functions takes single argument which
+ *                            is the text of the commands.
+ */
 export function init(onClick) {
   let submitButton = document.querySelector(".ToyRobot-submitButton");
   let inputElement = document.querySelector(".ToyRobot-input");
   submitButton.onclick = () => onClick(inputElement.value);
 }
 
+
+/**
+ * Shows output on screen
+ *
+ * @param  {object} output Array of text output strings to be shown.
+ */
 export function appendToLog(output) {
   let logElement = document.querySelector(".ToyRobot-log");
 
